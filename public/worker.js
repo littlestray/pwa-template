73332a765fb938b8e96d7ef03860da3bca9ab8fb
Chris Baldys <littlestraymusic@gmail.com>
2023-10-24 00:39:45 -0400

    Update worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,3 +1,19 @@
 self.addEventListener("fetch", (event) => {
     console.log(event)
 })
+
+addEventListener("message", (msg) => {
+    console.log(msg)
+    if (msg === "init") {
+        geoInit()
+        console.log(navigator)
+    }
+})
+
+function geoInit() {
+    if ("geolocation" in navigator) {
+        console.log("geo!!")
+      } else {
+        /* geolocation IS NOT available */
+      }
+}
